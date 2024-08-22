aafb1594bd9d3eb0a8c786c22beba9182b88d7e4
mehrdadsaadati <63453421+mehrdadsaadati@users.noreply.github.com>
2024-08-22 22:14:37 +0330

    fix: small file naming fix

--- tests/test_dir_maker.py
+++ tests/test_dir_maker.py
@@ -1,5 +1,4 @@
-import pytest
-from dir_maker_cli.dir_maker.dir_maker import create_directories
+from dir_maker_cli.dir_maker.cli import create_directories
 from dir_maker_cli.models.command import Command
 import os
 import shutil
